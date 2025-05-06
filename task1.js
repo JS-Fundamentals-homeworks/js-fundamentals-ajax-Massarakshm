78178c06b696c111412435ddca820ca4fa8f7b26
Massaracsh <krishtalka@gmail.com>
2025-05-06 16:39:12 +0300

    Part 11(Ajax)

--- task1.js
+++ task1.js
@@ -5,3 +5,22 @@
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
 // Перевірити за допомогою команди npm tests/task1.test.js 
+
+fetch('https://jsonplaceholder.typicode.com/users') // sample API
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => { 
+    const ul = document.querySelector("ul.usersList");
+    for(const user of data){
+        const li = document.createElement('li');
+li.textContent = user.name;
+ul.appendChild(li);
+    }
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
--- task2.js
+++ task2.js
@@ -5,3 +5,27 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+document.getElementById('getUserButton').addEventListener('click', () => {
+    const userName = document.getElementById('userNameInput').value;
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => { 
+        const userFiltered = data.filter((item) => {
+            return item.name === userName;
+        });
+        if (userFiltered.lenght !== 0) {
+            document.getElementById('userCity').textContent = userFiltered[0].address.city;
+        }
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+  
+});
